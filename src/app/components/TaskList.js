36a7364ba452a430609c61683f0e372f8ffce01e
t451076 <hooman.ebrahimi@deluxe.com>
2019-06-04 17:11:49 -0400

    initial env set up and architecture

---
+++ src/app/components/TaskList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TaskList = ({tasks, name}) => (
+    <div>
+        <h3>{name}</h3>
+        <div>
+            {tasks.map(task => <div>{task.name}</div>)}
+        </div>
+    </div>
+)
+
+
+const mapStateToProps = (state, ownProps) => {
+    let groupId = ownProps.id;
+    return {
+        name: ownProps.name,
+        id: groupId,
+        tasks: state.tasks.filter(task => task.group === groupId)
+    }
+}
+
+export default connect(mapStateToProps)(TaskList);
